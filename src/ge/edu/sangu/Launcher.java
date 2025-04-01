f2305e9ba53251bdef7e57f69b2591593173557f
vakho10 <vakho10@gmail.com>
2025-04-01 09:47:30 +0400

    Remove stupic laucher code

--- src/ge/edu/sangu/Launcher.java
+++ src/ge/edu/sangu/Launcher.java
@@ -1,26 +1,9 @@
 package ge.edu.sangu;
 
-import java.util.Scanner;
-
 public class Launcher {
-
-    private static void inputDefaultLoggingLevel() {
-        do {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Please enter default logging level. Choose from [DEBUG, INFO, ERROR]: ");
-                Level defaultLoggingLevel = Level.valueOf(scanner.nextLine());
-                Logger.setDefaultLevel(defaultLoggingLevel);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("Please enter valid logging level!");
-            }
-        } while (true);
-    }
+    private static final Logger log = new Logger(System.out);
 
     public static void main(String[] args) {
-        inputDefaultLoggingLevel();
-        Logger log = new Logger(System.out);
         log.info("Application started.");
         log.debug("Something is " + 12);
         try {
