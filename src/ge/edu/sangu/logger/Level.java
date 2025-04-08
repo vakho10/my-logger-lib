381270da947b4db30e0ce3673d8e680863f5c357
vakho10 <vakho10@gmail.com>
2025-04-08 09:30:09 +0400

    Add JavaDoc comments

--- src/ge/edu/sangu/logger/Level.java
+++ src/ge/edu/sangu/logger/Level.java
@@ -1,5 +1,11 @@
 package ge.edu.sangu.logger;
 
+/**
+ * This enum contains logging level constants.
+ *
+ * @author Vakho10
+ * @see <a href="https://technology.amis.nl/software-development/software-engineering/write-debug-level-logs-on-error-print-detailed-logging-only-in-exceptional-circumstances/">Logging Levels</a>
+ */
 public enum Level {
     DEBUG, INFO, WARN, ERROR, FATAL
 }
--- src/ge/edu/sangu/logger/Logger.java
+++ src/ge/edu/sangu/logger/Logger.java
@@ -4,14 +4,38 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.time.LocalDateTime;
 
+/**
+ * Generic logger class, which may be used as a parent class for future loggers.
+ *
+ * @author Vakho10
+ */
 public class Logger {
+
+    /**
+     * Logging level (INFO by default).
+     */
     private Level level = Level.INFO;
+
+    /**
+     * Customizable output stream where we flush our logging messages.
+     */
     private final PrintStream out;
 
+    /**
+     * Constructor that takes any output stream.
+     *
+     * @param out output stream where messages will go
+     */
     public Logger(OutputStream out) {
         this.out = new PrintStream(out);
     }
 
+    /**
+     * Constructor that takes logger's logging level with output stream.
+     *
+     * @param level logger's current logging level
+     * @param out   output stream where messages will go
+     */
     public Logger(Level level, PrintStream out) {
         this(out);
         this.level = level;
@@ -21,6 +45,12 @@ public class Logger {
         return level;
     }
 
+    /**
+     * Sets current logger's logging level.
+     *
+     * @param level
+     * @throws IllegalArgumentException If logging level input is null
+     */
     public void setLevel(Level level) {
         if (level == null) {
             throw new IllegalArgumentException("Default logging level must not be null");
@@ -28,7 +58,17 @@ public class Logger {
         this.level = level;
     }
 
+    /**
+     * Generic print method that takes logging level along with message.
+     *
+     * @param level   message level
+     * @param message log message
+     * @throws IllegalArgumentException If level is null, or message is null or blank
+     */
     public void print(Level level, String message) {
+        if (level == null) {
+            throw new IllegalArgumentException("Logging level must not be null");
+        }
         if (message == null || message.isBlank()) {
             throw new IllegalArgumentException("Log message must not be null or blank");
         }
